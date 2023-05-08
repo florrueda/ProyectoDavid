dfdd9b537ea682906a8d3e781f0275db3cda91b0
Flor-Rueda <flor.rueda@gmail.com>
2023-05-08 18:48:55 -0300

    Ajuste animacion home

--- main.js
+++ main.js
@@ -4,21 +4,25 @@
   /**
    * Preloader
    */
-  let preloader = select('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove()
-    });
-  }
+  // let preloader = select('#preloader');
+  // if (preloader) {
+  //   window.addEventListener('load', () => {
+  //     preloader.remove()
+  //   });
+  // }
 
-   /**
-   * Type effect
-   */
-   const typed = new Typed('.typed', {
+
+  const typed = new Typed('.typed', {
     strings: ['mágicas', 'innovadoras','ágiles', 'creativas'],
+    loop: true,
     typeSpeed: 50,
     backSpeed: 50,
     backDelay: 2000,
+    onComplete: function() {
+      document.querySelectorAll('.underline').forEach(function(element) {
+        element.classList.add('animate');
+      });
+    }
   });
 
 
